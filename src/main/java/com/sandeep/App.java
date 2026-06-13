2369f0428642d104050919d8cd191db9d2e1e4d0
ManiSandeepDoradla <112079522+ManiSandeepDoradla@users.noreply.github.com>
2026-06-13 16:43:41 +0530

    Create Spring Boot application with a home endpoint

---
+++ src/main/java/com/sandeep/App.java
@@ -0,0 +1,20 @@
+package com.sandeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "<h1>POC-1: DevSecOps Java App Deployed via Maven, SonarQube, OWASP, & Trivy!</h1>";
+    }
+}
